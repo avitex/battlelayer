2093cf02f2a66de7485beb4cc49adf86e2bd7d11
avitex <theavitex@gmail.com>
2019-10-03 17:18:32 +1000

    Use bytes and start impl tokio client

--- src/packet.rs
+++ src/packet.rs
@@ -1,112 +1,116 @@
-use std::{io, str};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
+use std::str;
+use std::io::Cursor;
 
 const PACKET_MAX_SIZE: usize = 16384;
 const PACKET_MAX_WORDS: usize = 256;
 const PACKET_HEADER_SIZE: usize = 12;
 const PACKET_WORD_MIN_SIZE: usize = 5;
 const PACKET_WORD_MAX_SIZE: usize = PACKET_MAX_SIZE - (PACKET_HEADER_SIZE + PACKET_WORD_MIN_SIZE);
-const PACKET_SEQ_CLIENT_MASK_U8: u8 = 0x80;
-const PACKET_SEQ_RESPON_MASK_U8: u8 = 0x40;
 const PACKET_SEQ_CLIENT_MASK_U32: u32 = 0x8000_0000;
 const PACKET_SEQ_RESPON_MASK_U32: u32 = 0x4000_0000;
 const PACKET_SEQ_HEADER_MASK_U32: u32 = PACKET_SEQ_CLIENT_MASK_U32 | PACKET_SEQ_RESPON_MASK_U32;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-fn read_exact(mut r: impl io::Read, buf: &mut [u8]) -> Result<(), PacketError> {
-    Ok(r.read_exact(buf)?)
+macro_rules! with_some {
+    ($e:expr) => {
+        match $e {
+            Some(val) => val,
+            None => {
+                return Ok(None);
+            }
+        }
+    };
 }
 
-fn read_quad(r: impl io::Read) -> Result<[u8; 4], PacketError> {
-    let mut buf = [0u8; 4];
-    read_exact(r, &mut buf)?;
-    Ok(buf)
-}
-
-fn read_u32_size(r: impl io::Read, max: usize) -> Result<usize, PacketError> {
-    let buf = read_quad(r)?;
-    let val = u32::from_le_bytes(buf) as usize;
-    if val > max {
-        return Err(PacketError::InvalidSize(val));
+fn read_u32_as_bounded_usize(
+    mut buf: impl Buf,
+    min: usize,
+    max: usize,
+) -> Result<Option<usize>, PacketError> {
+    if buf.remaining() < 4 {
+        Ok(None)
+    } else {
+        let val = buf.get_u32_le() as usize;
+        if val < min || val > max {
+            return Err(PacketError::InvalidSize(val));
+        }
+        Ok(Some(val))
     }
-    Ok(val)
 }
 
-fn read_packet<'s>(
-    mut r: impl io::Read,
-    scratch: &'s mut Vec<u8>,
-) -> Result<Packet<'s>, PacketError> {
-    // Read the packet sequence.
-    let seq_buf = read_quad(&mut r)?;
-    let seq = PacketSequence::from_raw(seq_buf);
+pub fn read_packet(
+    buf: &mut BytesMut,
+) -> Result<Option<Packet>, PacketError> {
+    // Return early if we can't met the packet header size.
+    if buf.len() < PACKET_HEADER_SIZE {
+        return Ok(None);
+    }
+    let header_buf = buf.split_to(PACKET_HEADER_SIZE);
+    let mut header_cur = Cursor::new(header_buf.as_ref());
+    // Read the packet seq.
+    let seq = PacketSequence::from_raw(header_cur.get_u32_le());
     // Read the packet size.
-    let size = read_u32_size(&mut r, PACKET_MAX_SIZE)?;
-    // Validate it is the header size or larger.
-    if size < PACKET_HEADER_SIZE {
-        return Err(PacketError::InvalidSize(size));
-    }
-    // Read the number of words.
-    let num_word = read_u32_size(&mut r, PACKET_MAX_WORDS)?;
-    // Init container for words.
-    let mut words = Vec::with_capacity(num_word);
+    let size = with_some!(read_u32_as_bounded_usize(
+        &mut header_cur,
+        PACKET_HEADER_SIZE,
+        PACKET_MAX_SIZE
+    )?);
+    // Read the word count.
+    let word_count = with_some!(read_u32_as_bounded_usize(&mut header_cur, 0, PACKET_MAX_WORDS)?);
+    // Create container for words.
+    let mut words = Vec::with_capacity(word_count);
     // Calculate the body size.
     let body_size = size - PACKET_HEADER_SIZE;
-    // Read the packet body into the scratch space.
-    unsafe {
-        // Firstly we clear the scratch space.
-        scratch.clear();
-        // Now we reserve enough space to write to.
-        scratch.reserve(body_size);
-        // We set the size of the space to that of which we reserved.
-        // This memory is uninitialized, however we will write to
-        // it in the next step.
-        scratch.set_len(body_size);
-        // Read the body bytes into the scratch space.
-        read_exact(r, scratch.as_mut_slice())?;
+    // Return early if we can't met the packet body size.
+    if buf.len() < body_size {
+        buf.unsplit(header_buf);
+        return Ok(None);
     }
-    // Read words from the scratch space.
-    let mut scratch_cursor = 0;
-    for _ in 0..num_word {
-        if (scratch.len() - scratch_cursor) < PACKET_WORD_MIN_SIZE {
+    // Read the body bytes.
+    let mut body_buf = buf.split_to(body_size);
+    // Read packet words.
+    for _ in 0..word_count {
+        if body_buf.len() < 4 {
             return Err(PacketError::Malformed);
         }
-        let word_size_end = scratch_cursor + 4;
-        let word_size = read_u32_size(
-            &scratch[scratch_cursor..word_size_end],
-            PACKET_WORD_MAX_SIZE,
-        )?;
-        if (scratch.len() - scratch_cursor) < word_size {
+        let word_size_buf = body_buf.split_to(4);
+        let word_size = read_u32_as_bounded_usize(
+            &mut Cursor::new(word_size_buf.as_ref()),
+            PACKET_WORD_MIN_SIZE,
+            PACKET_WORD_MAX_SIZE
+        )?.ok_or(PacketError::Malformed)?;
+        if body_buf.len() < word_size + 1 {
             return Err(PacketError::Malformed);
         }
-        let word_end = word_size_end + word_size;
-        let word_bytes = &scratch[word_size_end..word_end];
+        let word_buf = body_buf.split_to(word_size);
         // Check the trailing null character.
-        if scratch[word_end] != 0 {
+        if body_buf.split_to(1).as_ref() != &[0] {
             return Err(PacketError::Malformed);
         }
-        scratch_cursor = word_end + 1;
-        words.push(PacketWord::from_raw(word_bytes)?);
+        words.push(PacketWord::from_raw(word_buf.freeze())?);
     }
-    Ok(Packet { seq, words })
+    Ok(Some(Packet { seq, words }))
 }
 
-fn write_size_u32(mut w: impl io::Write, size: usize) -> Result<(), PacketError> {
+fn write_size_u32(buf: &mut BytesMut, size: usize) -> Result<(), PacketError> {
     if size > (u32::max_value() as usize) {
         return Err(PacketError::InvalidSize(size));
     }
-    let size_bytes = (size as u32).to_le_bytes();
-    w.write(&size_bytes[..])?;
-    Ok(())
+    Ok(buf.put_u32_le(size as u32))
 }
 
-fn write_packet<'a>(mut w: impl io::Write, p: &Packet<'a>) -> Result<(), PacketError> {
-    w.write(p.seq.as_bytes())?;
-    write_size_u32(&mut w, p.byte_size())?;
-    write_size_u32(&mut w, p.words.len())?;
-    for word in p.words.iter() {
-        write_size_u32(&mut w, word.byte_size())?;
-        w.write(word.as_bytes())?;
-        w.write(&[0])?;
+pub fn write_packet(buf: &mut BytesMut, packet: Packet) -> Result<(), PacketError> {
+    let packet_size = packet.byte_size();
+    buf.reserve(packet_size);
+    buf.put_u32_le(packet.seq.to_raw());
+    write_size_u32(buf, packet_size)?;
+    write_size_u32(buf, packet.words.len())?;
+    for word in packet.words.into_iter() {
+        write_size_u32(buf, word.byte_size())?;
+        buf.put(word.into_bytes());
+        buf.put_u8(0);
     }
     Ok(())
 }
@@ -114,12 +118,12 @@ fn write_packet<'a>(mut w: impl io::Write, p: &Packet<'a>) -> Result<(), PacketE
 ////////////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug)]
-pub struct Packet<'a> {
+pub struct Packet {
     pub seq: PacketSequence,
-    pub words: Vec<PacketWord<'a>>,
+    pub words: Vec<PacketWord>,
 }
 
-impl<'a> Packet<'a> {
+impl Packet {
     /// Calculates the total size of the packet.
     pub fn byte_size(&self) -> usize {
         let words_byte_size: usize = self
@@ -136,18 +140,10 @@ impl<'a> Packet<'a> {
 /// Represents a failure while handling a packet.
 #[derive(Debug)]
 pub enum PacketError {
-    Io(io::Error),
-    BrokenPipe,
     Malformed,
     InvalidSize(usize),
     InvalidWordChar(u8),
     InvalidSequenceNumber,
-}
-
-impl From<io::Error> for PacketError {
-    fn from(err: io::Error) -> Self {
-        PacketError::Io(err)
-    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -170,7 +166,7 @@ pub enum PacketOrigin {
 
 #[derive(Debug)]
 pub struct PacketSequence {
-    bytes: [u8; 4],
+    raw: u32,
 }
 
 impl PacketSequence {
@@ -185,19 +181,24 @@ impl PacketSequence {
             if origin == PacketOrigin::Client {
                 seq |= PACKET_SEQ_CLIENT_MASK_U32;
             }
-            Ok(Self::from_raw(seq.to_le_bytes()))
+            Ok(Self::from_raw(seq))
         }
     }
 
     /// Create a new packet sequence from it's raw protocol
     /// representation.
-    pub fn from_raw(bytes: [u8; 4]) -> Self {
-        Self { bytes }
+    pub fn from_raw(raw: u32) -> Self {
+        Self { raw }
+    }
+
+    /// Returns the raw protocol representation.
+    pub fn to_raw(&self) -> u32 {
+        self.raw
     }
 
     /// Returns the origin of the packet (client/server).
     pub fn origin(&self) -> PacketOrigin {
-        if (self.bytes[3] & PACKET_SEQ_CLIENT_MASK_U8) != 0 {
+        if (self.raw & PACKET_SEQ_CLIENT_MASK_U32) != 0 {
             PacketOrigin::Client
         } else {
             PacketOrigin::Server
@@ -206,7 +207,7 @@ impl PacketSequence {
 
     /// Returns the kind of packet (request/response).
     pub fn kind(&self) -> PacketKind {
-        if (self.bytes[3] & PACKET_SEQ_RESPON_MASK_U8) != 0 {
+        if (self.raw & PACKET_SEQ_RESPON_MASK_U32) != 0 {
             PacketKind::Response
         } else {
             PacketKind::Request
@@ -215,12 +216,7 @@ impl PacketSequence {
 
     /// Returns the packet sequence number.
     pub fn number(&self) -> u32 {
-        u32::from_le_bytes(self.bytes) & !PACKET_SEQ_HEADER_MASK_U32
-    }
-
-    /// Returns the raw protocol representation.
-    pub fn as_bytes(&self) -> &[u8] {
-        &self.bytes[..]
+        self.raw & !PACKET_SEQ_HEADER_MASK_U32
     }
 }
 
@@ -228,17 +224,17 @@ impl PacketSequence {
 
 /// Represents a Packet word.
 #[derive(Debug, PartialEq)]
-pub struct PacketWord<'a> {
-    bytes: &'a [u8],
+pub struct PacketWord {
+    bytes: Bytes,
 }
 
-impl<'a> PacketWord<'a> {
-    pub fn new(word: &'a str) -> Result<Self, PacketError> {
-        Self::from_raw(word.as_bytes())
+impl PacketWord {
+    pub fn new(word: &str) -> Result<Self, PacketError> {
+        Self::from_raw(Bytes::from(word.as_bytes()))
     }
 
-    pub fn from_raw(bytes: &'a [u8]) -> Result<Self, PacketError> {
-        if let Some(invalid_char) = bytes.into_iter().find(|b| !Self::is_valid_char(**b)) {
+    fn from_raw(bytes: Bytes) -> Result<Self, PacketError> {
+        if let Some(invalid_char) = bytes.as_ref().iter().find(|b| !Self::is_valid_char(**b)) {
             Err(PacketError::InvalidWordChar(*invalid_char))
         } else {
             Ok(Self { bytes })
@@ -251,10 +247,14 @@ impl<'a> PacketWord<'a> {
     }
 
     pub fn as_str(&self) -> &str {
-        unsafe { str::from_utf8_unchecked(self.bytes) }
+        unsafe { str::from_utf8_unchecked(self.as_bytes()) }
     }
 
     pub fn as_bytes(&self) -> &[u8] {
+        self.bytes.as_ref()
+    }
+
+    pub fn into_bytes(self) -> Bytes {
         self.bytes
     }
 
@@ -265,51 +265,6 @@ impl<'a> PacketWord<'a> {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-
-pub struct PacketReader<R: io::Read> {
-    r: R,
-    broken: bool,
-    scratch: Vec<u8>,
-}
-
-impl<R: io::Read> PacketReader<R> {
-    pub fn new(r: R) -> Self {
-        Self {
-            r,
-            broken: false,
-            scratch: Vec::with_capacity(4096),
-        }
-    }
-
-    pub fn read_packet<'p>(&'p mut self) -> Result<Packet<'p>, PacketError> {
-        if self.broken {
-            return Err(PacketError::BrokenPipe);
-        }
-        match read_packet(&mut self.r, &mut self.scratch) {
-            Ok(p) => Ok(p),
-            Err(err) => {
-                self.broken = true;
-                Err(err)
-            }
-        }
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-pub struct PacketWriter<W: io::Write> {
-    w: W,
-}
-
-impl<W: io::Write> PacketWriter<W> {
-    pub fn new(w: W) -> Self {
-        Self { w }
-    }
-
-    pub fn write_packet<'a>(&mut self, p: &Packet<'a>) -> Result<(), PacketError> {
-        write_packet(&mut self.w, p)
-    }
-}
 
 #[cfg(test)]
 mod tests {
@@ -331,8 +286,7 @@ mod tests {
             // word "world"
             5, 0, 0, 0, b'w', b'o', b'r', b'l', b'd', 0,
         ];
-        let mut scratch = Vec::new();
-        let packet = read_packet(&packet_bytes[..], &mut scratch).unwrap();
+        let packet = read_packet(&mut BytesMut::from(&packet_bytes[..])).unwrap().unwrap();
         assert_eq!(packet.seq.kind(), PacketKind::Request);
         assert_eq!(packet.seq.origin(), PacketOrigin::Client);
         assert_eq!(
@@ -342,8 +296,8 @@ mod tests {
                 PacketWord::new("world").unwrap(),
             ]
         );
-        let mut out =  vec![0u8; packet_bytes.len()];
-        write_packet(&mut out[..], &packet).unwrap();
+        let mut out = BytesMut::with_capacity(packet_bytes.len());
+        write_packet(&mut out, packet).unwrap();
         assert_eq!(&out[..], &packet_bytes[..]);
     }
 
@@ -361,7 +315,8 @@ mod tests {
 
     #[test]
     fn client_packet_sequence_test() {
-        let seq = PacketSequence::from_raw([0b0000_00000, 0b0000_0000, 0b0000_0000, 0b1000_0000]);
+        let seq_bytes = u32::from_le_bytes([0b0000_00000, 0b0000_0000, 0b0000_0000, 0b1000_0000]);
+        let seq = PacketSequence::from_raw(seq_bytes);
         assert_eq!(seq.origin(), PacketOrigin::Client);
         assert_eq!(seq.number(), 0u32);
         assert_eq!(seq.kind(), PacketKind::Request);
@@ -369,7 +324,8 @@ mod tests {
 
     #[test]
     fn server_packet_sequence_test() {
-        let seq = PacketSequence::from_raw([0b0000_0000, 0b0000_0000, 0b0000_0000, 0b0100_0000]);
+        let seq_bytes = u32::from_le_bytes([0b0000_00000, 0b0000_0000, 0b0000_0000, 0b0100_0000]);
+        let seq = PacketSequence::from_raw(seq_bytes);
         assert_eq!(seq.origin(), PacketOrigin::Server);
         assert_eq!(seq.kind(), PacketKind::Response);
         assert_eq!(seq.number(), 0u32);
